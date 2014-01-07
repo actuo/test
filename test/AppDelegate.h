242585e94530703579ef204fbc114ab65e521dfb
chao <zhouchaosy@gmail.com>
2014-01-07 11:47:25 -0800

    Initial commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Chao on 1/7/14.
+//  Copyright (c) 2014 Chao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  test
+//
+//  Created by Chao on 1/7/14.
+//  Copyright (c) 2014 Chao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
