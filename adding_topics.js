05a78c9b00f930bfff345ce260c89cc18c334a56
actuo <zhouchaosy@gmail.com>
2015-04-15 14:33:54 -0700

    Update adding_topics.js

--- adding_topics.js
+++ adding_topics.js
@@ -74,7 +74,7 @@ function renderInfo() {
 //    	var frame_name = 'topic'+i;
 //    	html='<iframe width="100%" src="'+ topics[i] + '" name="'+frame_name+'" height="300"></iframe>';
 //	document.getElementById('body').innerHTML = html;
-    }
+//    }
  
     /** Adjust window size dynamically */
     gadgets.window.adjustHeight();
